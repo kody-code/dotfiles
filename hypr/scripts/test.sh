5d227095cceb39880530bf75ae511a41ec33454a
kody <wangzhaojie219@163.com>
2025-12-15 10:15:08 +0800

    fix(hypr): 删除不必要的测试文件

--- hypr/scripts/test.sh
+++
@@ -1,3 +0,0 @@
-WALLPAPER_DIR="$HOME/Pictures/Wallpapers/"
-NEXT_WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.gif" -o -name "*.webp" \) 2>/dev/null | shuf -n1)
-echo "$NEXT_WALLPAPER"
